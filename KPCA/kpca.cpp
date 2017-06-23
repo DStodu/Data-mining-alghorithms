4466bf9639ec88103bd5a958569092db280272ca
PanOmeleta <hmnt@centrum.cz>
2017-06-23 20:28:47 +0200

    minor fixes, doxygen

--- KPCA/kpca.cpp
+++ KPCA/kpca.cpp
@@ -1,6 +1,6 @@
 #include "kpca.h"
 
-Eigen::MatrixXd GetData(std::string filename)  /**  Loads data into Eigen matrix, data formated as .csv with semicolon as a delimiter **/
+Eigen::MatrixXd GetData(std::string filename)  
 {
     std::fstream file(filename.c_str(), std::fstream::in);
     if(!file)
@@ -52,7 +52,7 @@ Eigen::MatrixXd GetData(std::string filename)  /**  Loads data into Eigen matrix
     return input;
 }
 
-Eigen::MatrixXd TransformData(Eigen::MatrixXd gKernel, Eigen::MatrixXd eigenVectors, int components)  /** Returns new data representation based on selected vectors **/
+Eigen::MatrixXd TransformData(Eigen::MatrixXd gKernel, Eigen::MatrixXd eigenVectors, int components) 
 {
     Eigen::MatrixXd transformed = Eigen::MatrixXd::Zero(gKernel.rows(),components);
 
@@ -71,78 +71,11 @@ Eigen::MatrixXd TransformData(Eigen::MatrixXd gKernel, Eigen::MatrixXd eigenVect
     return transformed;
 }
 
-void KRepresentation(Eigen::MatrixXd gKernel, std::string filename)  /** Returns new data representation, number of eigenvectors is suggested based on 90% variance **/
+
+void KRepresentationValues(Eigen::MatrixXd gKernel, int components, std::string filename)  
 {
     Eigen::EigenSolver<Eigen::MatrixXd> ces;
-    ces.compute(gKernel);
-
-    Eigen::MatrixXd eigenVectors;
-    Eigen::VectorXd eigenValues;
-
-    eigenVectors = ces.eigenvectors().real();
-    eigenValues = ces.eigenvalues().real();
-
-    float sum = 0;
-
-    for(int i = 0; i < eigenValues.size(); i++)
-    {
-        sum+=std::abs(eigenValues(i));
-    }
-
-    float variance = 0;
-    int suggCount = 0;
-    int counter = 0;
-    int components = 0;
-
-    for(int i = 0; i < eigenValues.size(); i++)
-    {
-        variance+=std::abs(eigenValues(i))/sum;
-        suggCount++;
-        counter++;
-        if(variance >= 0.9)
-            break;
-    }
-
-    float suggPlusOne, suggPlusTwo;
-
-    suggPlusOne = eigenValues(counter)/sum;
-    suggPlusTwo = eigenValues(counter+1)/sum;
-
-    std::cout << "Suggested number of eigenvalues with a variance of at least 90% :" << suggCount << std::endl;
-    std::cout << "Another value would change the variance by: " << suggPlusOne << std::endl;
-    std::cout << "Another value would change the variance by: " << suggPlusTwo << std::endl;
-
-    std::cout << "Enter the desired number of eigenvalues: ";
-    std::cin >> components;
-
-    if(components <= 0)
-    {
-        std::cerr << "Negative or zero number of components!" << std::endl;
-        exit(-2);
-    }
-
-
-    Eigen::MatrixXd transformed = TransformData(gKernel,eigenVectors,components);
-
-    std::fstream file;
-    file.open(filename.c_str(),std::ios::out | std::ios::trunc);
-
-    for(int i = 0; i < transformed.rows() ; i++)
-    {
-        for(int j = 0; j < transformed.cols(); j++)
-        {
-            file << transformed(i,j) << ";";
-        }
-        file << std::endl;
-    }
-    file.close();
-}
-
-
-void KRepresentationValues(Eigen::MatrixXd gKernel, int components, std::string filename)  /** Returns new data representation, number of used eigenvectors is taken from the function parameter -> user input, saves to file, ; as delimiter **/
-{
-    Eigen::EigenSolver<Eigen::MatrixXd> ces;
-    ces.compute(gKernel);						/** eigen values solver, + eigen vector both right **/
+    ces.compute(gKernel);						
 
     Eigen::MatrixXd eigenVectors;
     Eigen::VectorXd eigenValues;
@@ -166,7 +99,7 @@ void KRepresentationValues(Eigen::MatrixXd gKernel, int components, std::string 
     file.close();
 }
 
-Eigen::MatrixXd KRepresentationValues(Eigen::MatrixXd gKernel, int components)   /** Returns new data representation, number of used eigenvectors is taken from the function parameter -> user input, in eigen structure **/
+Eigen::MatrixXd KRepresentationValues(Eigen::MatrixXd gKernel, int components) 
 {
     Eigen::EigenSolver<Eigen::MatrixXd> ces;
     ces.compute(gKernel);
--- KPCA/kpca.h
+++ KPCA/kpca.h
@@ -1,10 +1,18 @@
 /**
-5.2016
-Kernel Principal Component Analysis
-Daniel Stodulka
-dstodu@gmail.com
-Eigen (http://eigen.tuxfamily.org/index.php?title=Main_Page) library used for matrix operations
-**/
+ * @file kpca.h
+ *
+ * @author Daniel Stodulka
+ *
+ * @date 2016
+ *
+ * @brief Basic KPCA functions, loading data, eigen decomposition
+ *
+ * Implementation of Kernel Principal Component Analysis as described in Dimensionality reduction a comparative review.<br>
+ * Eigen library is used for datastructres and eigen decomposition
+ *  
+ * @see http://eigen.tuxfamily.org/index.php?title=Main_Page
+ * @see https://lvdmaaten.github.io/publications/papers/TR_Dimensionality_Reduction_Review_2009.pdf
+ */
 
 #ifndef KPCA_H_INCLUDED
 #define KPCA_H_INCLUDED
@@ -17,15 +25,41 @@ Eigen (http://eigen.tuxfamily.org/index.php?title=Main_Page) library used for ma
 #include <string>
 
 /**
-
-Loading data
-Either returns new values or saves to file
-**/
-
+ * @brief Loading data into eigen matrix structure
+ * Loads data into eigen matrix structure, accepts csv format with ; as a delimiter
+ * @param filename Source data file filename
+ */
 Eigen::MatrixXd GetData(std::string);
+
+/**
+ * @brief Transforms data using selected number of eigenvalues/vectors
+ * Returns new data representation projected on selected eigenvectors
+ * @param kernel Kernel matrix
+ * @param vectors Eigenvectors
+ * @param components number of vectors to be used
+ */
+
+Eigen::MatrixXd TransformData(Eigen::MatrixXd, Eigen::MatrixXd, int);
+
+/**
+ * @brief Saves transformed data into file
+ * Returns new data representation projected on selected number of eigenvectors, saving into file in a csv structure
+ * @param kernel Kernel matrix
+ * @param components number of vectors to be used
+ * @param filename output file name
+ */
 
 void KRepresentationValues(Eigen::MatrixXd,int,std::string);
 
+/**
+ * @brief Returns transformed data in eigen matrix structure
+ *
+ * @param kernel Kernel matrix
+ * @param components number of vectors to be used
+ * 
+ */
+
 Eigen::MatrixXd KRepresentationValues(Eigen::MatrixXd,int);
+
 
 #endif // KPCA_H_INCLUDED
